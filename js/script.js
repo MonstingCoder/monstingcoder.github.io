67ec0e4adfe549590641b8abfdc91b3de26f70fc
monsco <muhammadvarza1905@gmail.com>
2024-11-05 11:34:54 +0700

    template (v0.02)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-document.getElementById('mode-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-});
+const modeToggle = document.getElementById('modeToggle');
+const icon = modeToggle.querySelector('i');
 
+modeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    icon.classList.toggle('fa-sun');
+    icon.classList.toggle('fa-moon');
+});
